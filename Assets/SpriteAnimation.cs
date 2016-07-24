4b2ae2eae5816ae45a7a5537c5d24ca73e870d3b
antonyip <anton_yip@hotmail.com>
2016-07-25 07:02:03 +0800

    testing of sprite animations

---
+++ Assets/SpriteAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpriteAnimation : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    int counter = 0;
+    void Awake()
+    {
+        // load all frames in fruitsSprites array
+        sprites = Resources.LoadAll<Sprite>("DustBunny");
+    }
+
+    void Start()
+    {
+        // create the object
+        GetComponent<Image>().sprite = sprites[0];
+    }
+
+    float totalTimeCounter = 0;
+
+    void Update()
+    {
+        totalTimeCounter += Time.deltaTime;
+        if (totalTimeCounter > 0.16f)
+        {
+            GetComponent<Image>().sprite = sprites[counter % sprites.Length];
+            totalTimeCounter = 0;
+            ++counter;
+        }
+    }
+}
