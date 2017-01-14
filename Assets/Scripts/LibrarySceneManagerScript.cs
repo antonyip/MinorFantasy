29a5d70913d968f08ee82131e31818d37a286736
bernardyip <bernard_yip40@hotmail.com>
2017-01-14 15:16:04 +0800

    Moved library button scripts to a new file

--- Assets/Scripts/LibrarySceneManagerScript.cs
+++ Assets/Scripts/LibrarySceneManagerScript.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class LibrarySceneManagerScript : MonoBehaviour {
 
-    public GameObject HeroView;
-    public GameObject EquipView;
-    public GameObject MobView;
-    public GameObject GambitView;
-    enum PanelName { HERO, EQUIP, MOB, GAMBIT };
-
     // Use this for initialization
     void Start () {
-        switchPanel(PanelName.HERO);
+        //Initialize and populate all panes
+        Google2u.HeroesData heroesData = Google2u.HeroesData.Instance;
+        foreach (string row in heroesData.rowNames)
+        {
+            Google2u.HeroesDataRow data = heroesData.GetRow(row);
+            Debug.Log(data);
+        }
+
+        //Set to Hero Scene First
+        LibrarySceneButtonScript.instance.GoToHero();
     }
 	
 	// Update is called once per frame
@@ -19,49 +22,4 @@ public class LibrarySceneManagerScript : MonoBehaviour {
 	
 	}
 
-    public void GoToHero()
-    {
-        switchPanel(PanelName.HERO);
-    }
-
-    public void GoToEquip()
-    {
-        switchPanel(PanelName.EQUIP);
-    }
-
-    public void GoToMob()
-    {
-        switchPanel(PanelName.MOB);
-    }
-
-    public void GoToGambit()
-    {
-        switchPanel(PanelName.GAMBIT);
-    }
-
-    void switchPanel(PanelName panelName)
-    {
-        HeroView.SetActive(false);
-        EquipView.SetActive(false);
-        MobView.SetActive(false);
-        GambitView.SetActive(false);
-        switch(panelName)
-        {
-            case PanelName.EQUIP:
-                EquipView.SetActive(true);
-                break;
-            case PanelName.GAMBIT:
-                GambitView.SetActive(true);
-                break;
-            case PanelName.HERO:
-                HeroView.SetActive(true);
-                break;
-            case PanelName.MOB:
-                MobView.SetActive(true);
-                break;
-            default:
-                Debug.Log("Error, invalid panel!");
-                break;
-        }
-    }
 }
