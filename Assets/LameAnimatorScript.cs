6cbb94c8c1c2c91b7861a2bc5b7f1aed0ffcd790
antonyip <anton_yip@hotmail.com>
2017-01-14 22:17:28 +0800

    AnimationsBranch

---
+++ Assets/LameAnimatorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LameAnimatorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GetComponent<Animator>().GetInteger("StateMachine") == 1)
+            {
+                GetComponent<Animator>().SetInteger("StateMachine", 0);
+            }
+            else
+            {
+                GetComponent<Animator>().SetInteger("StateMachine", 1);
+            }
+        }
+	}
+
+    public void ResetStateMachine()
+    {
+        GetComponent<Animator>().SetInteger("StateMachine", 0);
+    }
+}
