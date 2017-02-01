e3e6075027e819b9861cde0da42775d0a33cfe32
Anton.Yip <Anton.Yip@continental-corporation.com>
2017-02-01 11:09:55 +0800

    Support for booleans

--- GameDevTesting/Balancing/UnitTests.cs
+++ GameDevTesting/Balancing/UnitTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Balancing
 {
-    using Google2u;
+    using AntTool;
 
     [TestClass]
     public class UnitTests
@@ -11,26 +11,41 @@ namespace Balancing
         [TestMethod]
         public void FirstHeroIsNoHero()
         {
-            Assert.AreEqual(HeroesData.Instance.Rows[0]._Name, "No Hero");
+            Assert.AreEqual(HeroesData.instance.Rows[0]._Name, "No Hero");
         }
 
         [TestMethod]
         public void AllHeroStatsAreWithinLimits()
         {
-            const int LowerLimit = 50;
-            const int UpperLimit = 100;
-            foreach (var Hero in HeroesData.Instance.Rows)
+            const int LowerLimit = 200;
+            const int UpperLimit = 600;
+            foreach (var Hero in HeroesData.instance.Rows)
             {
-                int TotalValue = Hero._STR + Hero._DEX + Hero._LUK + Hero._INT;
-                bool TestPass = TestHelpers.ValueIsBetween(TotalValue, LowerLimit, UpperLimit);
-                Assert.IsTrue(TestPass, Hero._Name + " failed!");
+                if (Hero._TestFrame)
+                {
+                    int TotalValue = Hero._STR + Hero._DEX + Hero._LUK + Hero._INT;
+                    bool TestPass = TestHelpers.ValueIsBetween(TotalValue, LowerLimit, UpperLimit);
+                    Assert.IsTrue(TestPass, Hero._Name + " failed! Limit:" + TotalValue);
+                }
             }
         }
 
         [TestMethod]
-        public void AllHerosShouldOneShotTheirLevel1Self()
+        public void AllHeroesShouldKillTheirLevel1SelfIn2To5Hits()
         {
-            Assert.IsTrue(false);
+            const int LowerLimit = 2;
+            const int UpperLimit = 5;
+            const int MaxLevel = 10;
+            foreach (var Hero in HeroesData.instance.Rows)
+            {
+                if (Hero._TestFrame)
+                {
+                    int TotalValue = Hero._Att + Hero._AttGrowth * MaxLevel;
+                    float Hits = Hero._HP/TotalValue;
+                    bool TestPass = TestHelpers.ValueIsBetween(Hits, LowerLimit, UpperLimit);
+                    Assert.IsTrue(TestPass, Hero._Name + " failed! Hits:" + Hits);
+                }
+            }
         }
 
         [TestMethod]
